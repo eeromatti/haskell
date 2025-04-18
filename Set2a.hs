b38482e06e8c80645cafd12071dc3007348f2359
Eero Hallikainen <48011305+eeromatti@users.noreply.github.com>
2025-04-18 10:12:07 +0300

    Update Set2a.hs

--- Set2a.hs
+++ Set2a.hs
@@ -90,7 +90,7 @@ isPalindrome str = if reverse str == str then True else False
 --   palindromify "abracacabra" ==> "acaca"
 
 palindromify :: String -> String
-palindromify s = todo
+palindromify s = if reverse s == s then s else palindromify (tail (init s))
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement safe integer division, that is, a function that
