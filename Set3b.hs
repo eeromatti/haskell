b2a7548c0e7b38a694fd19130483040e99075909
Eero Hallikainen <48011305+eeromatti@users.noreply.github.com>
2025-04-25 16:18:46 +0300

    Update Set3b.hs

--- Set3b.hs
+++ Set3b.hs
@@ -149,8 +149,8 @@ merge :: [Int] -> [Int] -> [Int]
 merge [] ys = ys
 merge xs [] = xs
 merge (x:xs) (y:ys)
-  | x <= y   = x : y : merge xs ys
-  | x > y    = y : x : merge xs ys
+  | x <= y       = x : merge xs (y:ys)
+  | otherwise    = y : merge (x:xs) ys
 
 ------------------------------------------------------------------------------
 -- Ex 8: compute the biggest element, using a comparison function
